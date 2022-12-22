163f05ce012d2f14c5824b309f896bb29e9b7e91
aronwebsolution <44797871+VijayTahkur@users.noreply.github.com>
2022-12-22 10:38:39 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Layout from './Layout';
 import Dashboard from './screens/Dashboard';
 import PageLogin from './screens/PageLogin';
+import PageSignup from './screens/PageSignup';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<PageLogin />} />
+      <Route path='/signup' element={<PageSignup />} />
       <Route path='/' element={<Layout />}>
         <Route path='/home' element={<Dashboard />} />
       </Route>
